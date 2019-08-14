e4bc645ea0b6db977f5bf13dc4803704fe5a5e58
hwj0623 <hwj0623@naver.com>
2019-08-14 21:45:05 +0900

    초기 프로젝트 세팅

---
+++ FoodStoreBot/FoodStoreBot/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  FoodStoreBot
+//
+//  Created by hw on 14/08/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
